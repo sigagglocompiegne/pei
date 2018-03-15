9b8f553b6f19c00cadf15ec23afe243efee21e1c
SIG Agglo de Compiègne <sigagglocompiegne@users.noreply.github.com>
2018-03-15 10:59:45 +0100

    Update init_bd_pei_dependency.sql

--- init_bd_pei_dependency.sql
+++ init_bd_pei_dependency.sql
@@ -13,6 +13,7 @@ GRANT USAGE ON SCHEMA r_objet TO groupe_be;
 COMMENT ON SCHEMA r_objet
   IS 'Schéma contenant les objets géographiques virtuels métiers (zonages, lots, entités administratives, ...). Les données métiers (alphanumériques) sont stockées dans le schéma correspondant, et le lien s''effectue via la référence géographique. Une donnée géographique spécifique à un seul métier, reste dans le schéma du métier.';
 
+-- ################################################################# Domaine valeur - source du référentiel geom  ###############################################
 
 -- Table: r_objet.lt_src_geom
 
@@ -38,3 +39,32 @@ COMMENT ON TABLE r_objet.lt_src_geom
   IS 'Code permettant de décrire le type de référentiel géométrique';
 COMMENT ON COLUMN r_objet.lt_src_geom.code IS 'Code de la liste énumérée relative au type de référentiel géométrique';
 COMMENT ON COLUMN r_objet.lt_src_geom.valeur IS 'Valeur de la liste énumérée relative au type de référentiel géométrique';
+
+INSERT INTO lt_src_geom VALUES ('10', 'Cadastre');
+INSERT INTO lt_src_geom VALUES ('11', 'PCI vecteur');
+INSERT INTO lt_src_geom VALUES ('12', 'BD Parcellaire');
+INSERT INTO lt_src_geom VALUES ('13', 'RPCU');
+INSERT INTO lt_src_geom VALUES ('20', 'Ortho-images');
+INSERT INTO lt_src_geom VALUES ('21', 'Orthophotoplan IGN');
+INSERT INTO lt_src_geom VALUES ('22', 'Orthophotoplan partenaire');
+INSERT INTO lt_src_geom VALUES ('23', 'Orthophotoplan local');
+INSERT INTO lt_src_geom VALUES ('30', 'Filaire voirie');
+INSERT INTO lt_src_geom VALUES ('31', 'Route BDTopo');
+INSERT INTO lt_src_geom VALUES ('32', 'Route OSM');
+INSERT INTO lt_src_geom VALUES ('40', 'Cartes');
+INSERT INTO lt_src_geom VALUES ('41', 'Scan25');
+INSERT INTO lt_src_geom VALUES ('50', 'Lever');
+INSERT INTO lt_src_geom VALUES ('51', 'Plan topographique');
+INSERT INTO lt_src_geom VALUES ('52', 'PCRS');
+INSERT INTO lt_src_geom VALUES ('53', 'Trace GPS');
+INSERT INTO lt_src_geom VALUES ('60', 'Geocodage');
+INSERT INTO lt_src_geom VALUES ('71', 'Plan masse vectoriel');
+INSERT INTO lt_src_geom VALUES ('72', 'Plan masse redessiné');
+INSERT INTO lt_src_geom VALUES ('80', 'Thématique');
+INSERT INTO lt_src_geom VALUES ('81', 'Document d''urbanisme');
+INSERT INTO lt_src_geom VALUES ('82', 'Occupation du Sol');
+INSERT INTO lt_src_geom VALUES ('83', 'Thèmes BDTopo');
+INSERT INTO lt_src_geom VALUES ('99', 'Autre');
+INSERT INTO lt_src_geom VALUES ('00', 'Non renseigné');
+INSERT INTO lt_src_geom VALUES ('70', 'Plan masse');
+INSERT INTO lt_src_geom VALUES ('61', 'Base Adresse Locale');
