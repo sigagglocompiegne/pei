54957592b6d5706852dde36b8ae75491d536a8dc
SIG Agglo de Compiègne <sigagglocompiegne@users.noreply.github.com>
2018-03-15 10:47:08 +0100

    Update init_bd_pei_dependency.sql

--- init_bd_pei_dependency.sql
+++ init_bd_pei_dependency.sql
@@ -1,3 +1,19 @@
+-- Schema: r_objet
+
+-- DROP SCHEMA r_objet;
+
+CREATE SCHEMA r_objet
+  AUTHORIZATION postgres;
+
+GRANT ALL ON SCHEMA r_objet TO postgres;
+GRANT ALL ON SCHEMA r_objet TO groupe_sig WITH GRANT OPTION;
+GRANT USAGE ON SCHEMA r_objet TO groupe_eco;
+GRANT USAGE ON SCHEMA r_objet TO groupe_sig_stage WITH GRANT OPTION;
+GRANT USAGE ON SCHEMA r_objet TO groupe_be;
+COMMENT ON SCHEMA r_objet
+  IS 'Schéma contenant les objets géographiques virtuels métiers (zonages, lots, entités administratives, ...). Les données métiers (alphanumériques) sont stockées dans le schéma correspondant, et le lien s''effectue via la référence géographique. Une donnée géographique spécifique à un seul métier, reste dans le schéma du métier.';
+
+
 -- Table: r_objet.lt_src_geom
 
 -- DROP TABLE r_objet.lt_src_geom;
