bb54710e2c69c6fae6b2113a7bdcd3ea331ef21a
gregbodet <37414914+gregbodet@users.noreply.github.com>
2018-11-09 10:19:20 +0100

    Update init_bd_pei.sql

--- sql/init_bd_pei.sql
+++ sql/init_bd_pei.sql
@@ -53,6 +53,7 @@
 2018-06-12 : FV / implémentation des évolutions du modèle suite à réunion AFIGEO du 11/06/2018
 2018-08-07 : GB / Intégrationn des nouveaux rôles de connexion et des privilèges associés
 2018-11-05 : FV / Améliorations diverses et réorganisation du séquensage du code sql
+2018-11-09 : GB / Modification des vues xapps_geo_v_pei_ctr et geo_v_pei_ctr pour optimisation (modification des jointures et suppression du champ geom1 (ne sert à rien ici)
 
 Généralités sur le domaine métier PEI
 
@@ -1171,77 +1172,70 @@ ALTER TABLE m_defense_incendie.an_pei_ctr
 -- DROP VIEW m_defense_incendie.geo_v_pei_ctr;
 
 CREATE OR REPLACE VIEW m_defense_incendie.geo_v_pei_ctr AS 
- SELECT 
-  g.id_pei,
-  g.id_sdis,
-  g.verrou,
-  g.ref_terr,
-  e.lib_epci AS epci,
-  g.insee,
-  c.commune,
-  g.type_pei,
-  g.type_rd,
-  g.diam_pei,
-  g.raccord,
-  g.marque,
-  g.source_pei,
-  g.volume,
-  g.diam_cana,
-  g.etat_pei,
-  a.id_contrat,
-  a.press_stat,
-  a.press_dyn,
-  a.debit,
-  a.debit_max,
-  a.debit_r_ci,
-  a.etat_anom,
-  a.lt_anom,
-  a.etat_acces,
-  a.etat_sign,
-  a.etat_conf,
-  CASE 
-  WHEN a.etat_conf = 't' AND DATE_PART('year',(AGE(CURRENT_DATE,a.date_ct))) < 2 AND g.etat_pei ='02' THEN 't' -- cas ok pour la dispo (etat existant, conformité ok, controle < 2ans)
-  ELSE 'f'
-  END AS disponible,
-  CURRENT_DATE AS date_dispo,
-  a.date_mes,
-  a.date_ct,
-  a.ope_ct,
-  a.date_ro,
-  g.statut,
-  g.nom_etab,
-  g.gestion,
-  g.delegat,
-  g.cs_sdis,
-  g.situation,
-  g.observ,
-  g.photo_url,
-  g.src_pei,
-  g.x_l93,
-  g.y_l93,
-  g.src_geom,
-  g.src_date,
-  g.prec,
-  g.ope_sai,
-  g.date_sai,  
-  g.date_maj,
-  g.geom,
-  g.geom1
-
+ SELECT g.id_pei,
+    g.id_sdis,
+    g.verrou,
+    g.ref_terr,
+    lk.lib_epci AS epci,
+    g.insee,
+    lk.libgeo as commune,
+    g.type_pei,
+    g.type_rd,
+    g.diam_pei,
+    g.raccord,
+    g.marque,
+    g.source_pei,
+    g.volume,
+    g.diam_cana,
+    g.etat_pei,
+    a.id_contrat,
+    a.press_stat,
+    a.press_dyn,
+    a.debit,
+    a.debit_max,
+    a.debit_r_ci,
+    a.etat_anom,
+    a.lt_anom,
+    a.etat_acces,
+    a.etat_sign,
+    a.etat_conf,
+        CASE
+            WHEN a.etat_conf::text = 't'::text AND date_part('year'::text, age('now'::text::date::timestamp with time zone, a.date_ct::timestamp with time zone)) < 2::double precision AND g.etat_pei::text = '02'::text THEN 't'::text
+            ELSE 'f'::text
+        END AS disponible,
+    'now'::text::date AS date_dispo,
+    a.date_mes,
+    a.date_ct,
+    a.ope_ct,
+    a.date_ro,
+    g.statut,
+    g.nom_etab,
+    g.gestion,
+    g.delegat,
+    g.cs_sdis,
+    g.situation,
+    g.observ,
+    g.photo_url,
+    g.src_pei,
+    g.x_l93,
+    g.y_l93,
+    g.src_geom,
+    g.src_date,
+    g.prec,
+    g.ope_sai,
+    g.date_sai,
+    g.date_maj,
+    g.geom
    FROM m_defense_incendie.geo_pei g
-   LEFT JOIN m_defense_incendie.an_pei_ctr a ON a.id_pei = g.id_pei
-   LEFT JOIN r_osm.geo_v_osm_commune_apc as c ON g.insee = c.insee
--- jointure à 2 niveaux pour récupération du nom de l'EPCI
-   LEFT JOIN r_administratif.an_geo as lk ON lk.insee = g.insee
-   LEFT JOIN r_osm.geo_osm_epci as e ON e.cepci = lk.epci;
-
+     NATURAL JOIN m_defense_incendie.an_pei_ctr a 
+     INNER JOIN r_administratif.an_geo lk ON lk.insee::text = g.insee::text;
 
 ALTER TABLE m_defense_incendie.geo_v_pei_ctr
   OWNER TO sig_create;
+GRANT ALL ON TABLE m_defense_incendie.geo_v_pei_ctr TO sig_create;
+GRANT ALL ON TABLE m_defense_incendie.geo_v_pei_ctr TO create_sig;
+GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE m_defense_incendie.geo_v_pei_ctr TO edit_sig;
 GRANT SELECT ON TABLE m_defense_incendie.geo_v_pei_ctr TO read_sig;
-GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE m_defense_incendie.geo_v_pei_ctr TO edit_sig;
-GRANT ALL ON TABLE m_defense_incendie.geo_v_pei_ctr TO create_sig;					   
-	
 COMMENT ON VIEW m_defense_incendie.geo_v_pei_ctr
   IS 'Vue éditable destinée à la modification des données relatives aux PEI et aux contrôles';
 
@@ -1252,80 +1246,74 @@ COMMENT ON VIEW m_defense_incendie.geo_v_pei_ctr
 -- DROP VIEW x_apps.xapps_geo_v_pei_ctr;
 
 CREATE OR REPLACE VIEW x_apps.xapps_geo_v_pei_ctr AS 
- SELECT 
-  g.id_pei,
-  g.id_sdis,
-  g.verrou,
-  g.ref_terr,
-  e.lib_epci AS epci,
-  g.insee,
-  c.commune,
-  g.type_pei,
-  g.type_rd,
-  g.diam_pei,
-  g.raccord,
-  g.marque,
-  g.source_pei,
-  g.volume,
-  g.diam_cana,
-  g.etat_pei,
-  a.id_contrat,
-  a.press_stat,
-  a.press_dyn,
-  a.debit,
-  a.debit_max,
-  a.debit_r_ci,
-  a.etat_anom,
-  a.lt_anom,
-  a.etat_acces,
-  a.etat_sign,
-  a.etat_conf,
-  CASE 
-  WHEN a.etat_conf = 't' AND DATE_PART('year',(AGE(CURRENT_DATE,a.date_ct))) < 2 AND g.etat_pei ='02' THEN 't' -- cas ok pour la dispo (etat existant, conformité ok, controle < 2ans)
-  ELSE 'f'
-  END AS disponible,
-  CURRENT_DATE AS date_dispo,
-  a.date_mes,
-  a.date_ct,
-  a.ope_ct,
-  a.date_ro,
-  g.statut,
-  g.nom_etab,
-  g.gestion,
-  g.delegat,
-  g.cs_sdis,
-  g.situation,
-  g.observ,
-  g.photo_url,
-  g.src_pei,
-  g.x_l93,
-  g.y_l93,
-  g.src_geom,
-  g.src_date,
-  g.prec,
-  g.ope_sai,
-  g.date_sai,  
-  g.date_maj,
-  g.geom,
-  g.geom1
+ SELECT g.id_pei,
+    g.id_sdis,
+    g.verrou,
+    g.ref_terr,
+    lk.lib_epci AS epci,
+    g.insee,
+    lk.libgeo as commune,
+    g.type_pei,
+    g.type_rd,
+    g.diam_pei,
+    g.raccord,
+    g.marque,
+    g.source_pei,
+    g.volume,
+    g.diam_cana,
+    g.etat_pei,
+    a.id_contrat,
+    a.press_stat,
+    a.press_dyn,
+    a.debit,
+    a.debit_max,
+    a.debit_r_ci,
+    a.etat_anom,
+    a.lt_anom,
+    a.etat_acces,
+    a.etat_sign,
+    a.etat_conf,
+       CASE
+            WHEN a.etat_conf::text = 't'::text AND date_part('year'::text, age('now'::text::date::timestamp with time zone, a.date_ct::timestamp with time zone)) < 2::double precision AND g.etat_pei::text = '02'::text THEN 't'::text
+            ELSE 'f'::text
+        END AS disponible,
+    'now'::text::date AS date_dispo,
+    a.date_mes,
+    a.date_ct,
+    a.ope_ct,
+    a.date_ro,
+    g.statut,
+    g.nom_etab,
+    g.gestion,
+    g.delegat,
+    g.cs_sdis,
+    g.situation,
+    g.observ,
+    g.photo_url,
+    g.src_pei,
+    g.x_l93,
+    g.y_l93,
+    g.src_geom,
+    g.src_date,
+    g.prec,
+    g.ope_sai,
+    g.date_sai,
+    g.date_maj,
+    g.geom
 
    FROM m_defense_incendie.geo_pei g
-   LEFT JOIN m_defense_incendie.an_pei_ctr a ON a.id_pei = g.id_pei
-   LEFT JOIN r_osm.geo_v_osm_commune_apc as c ON g.insee = c.insee
--- jointure à 2 niveaux pour récupération du nom de l'EPCI
-   LEFT JOIN r_administratif.an_geo as lk ON lk.insee = g.insee
-   LEFT JOIN r_osm.geo_osm_epci as e ON e.cepci = lk.epci;
+     NATURAL JOIN m_defense_incendie.an_pei_ctr a 
+     INNER JOIN r_administratif.an_geo lk ON lk.insee::text = g.insee::text;
 
 
 ALTER TABLE x_apps.xapps_geo_v_pei_ctr
   OWNER TO sig_create;
-GRANT SELECT ON TABLE x_apps.xapps_geo_v_pei_ctr TO read_sig;
-GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE x_apps.xapps_geo_v_pei_ctr TO edit_sig;
+GRANT ALL ON TABLE x_apps.xapps_geo_v_pei_ctr TO sig_create;
 GRANT ALL ON TABLE x_apps.xapps_geo_v_pei_ctr TO create_sig;
-
+GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE x_apps.xapps_geo_v_pei_ctr TO edit_sig;
+GRANT SELECT ON TABLE x_apps.xapps_geo_v_pei_ctr TO read_sig;
 COMMENT ON VIEW x_apps.xapps_geo_v_pei_ctr
   IS 'Vue applicative destinée à la modification des données PEI sur le patrimoine géré par le service mutualisé eau potable et la consultation des autres PEI';
-
 
 
 
