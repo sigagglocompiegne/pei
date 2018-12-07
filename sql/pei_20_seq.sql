e190d85b1ffb0bb9b88151abedb24f9dd1a79471
gregbodet <37414914+gregbodet@users.noreply.github.com>
2018-12-07 15:12:59 +0100

    Update pei_20_seq.sql

--- sql/pei_20_seq.sql
+++ sql/pei_20_seq.sql
@@ -6,3 +6,78 @@
 /* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
 /* Auteur : FLorent Vanhoutte */
 /* Participant : Grégory Bodet */
+
+
+
+-- ################################################################# Séquence sur domaine valeur ouvert - contrat  ###############################################
+
+
+-- Sequence: m_defense_incendie.lt_pei_id_contrat_seq
+
+-- DROP SEQUENCE m_defense_incendie.lt_pei_id_contrat_seq;
+
+CREATE SEQUENCE m_defense_incendie.lt_pei_id_contrat_seq
+  INCREMENT 1
+  MINVALUE 0
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+ALTER TABLE m_defense_incendie.lt_pei_id_contrat_seq
+  OWNER TO sig_create;
+GRANT ALL ON SEQUENCE m_defense_incendie.lt_pei_id_contrat_seq TO sig_create;
+GRANT SELECT, USAGE ON SEQUENCE m_defense_incendie.lt_pei_id_contrat_seq TO public;
+
+
+-- ################################################################# Séquence sur domaine valeur ouvert - marque  ###############################################
+
+-- Sequence: m_defense_incendie.lt_pei_marque_seq
+
+-- DROP SEQUENCE m_defense_incendie.lt_pei_marque_seq;
+
+CREATE SEQUENCE m_defense_incendie.lt_pei_marque_seq
+  INCREMENT 1
+  MINVALUE 0
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+ALTER TABLE m_defense_incendie.lt_pei_marque_seq
+  OWNER TO sig_create;
+GRANT ALL ON SEQUENCE m_defense_incendie.lt_pei_marque_seq TO sig_create;
+GRANT SELECT, USAGE ON SEQUENCE m_defense_incendie.lt_pei_marque_seq TO public;
+
+
+-- ################################################################# Séquence sur domaine valeur ouvert - délégataire  ###############################################
+
+-- Sequence: m_defense_incendie.lt_pei_delegat_seq
+
+-- DROP SEQUENCE m_defense_incendie.lt_pei_delegat_seq;
+
+CREATE SEQUENCE m_defense_incendie.lt_pei_delegat_seq
+  INCREMENT 1
+  MINVALUE 0
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+ALTER TABLE m_defense_incendie.lt_pei_delegat_seq
+  OWNER TO sig_create;
+GRANT ALL ON SEQUENCE m_defense_incendie.lt_pei_delegat_seq TO sig_create;
+GRANT SELECT, USAGE ON SEQUENCE m_defense_incendie.lt_pei_delegat_seq TO public;
+
+
+-- ################################################################# Séquence sur domaine valeur ouvert - raccord  ###############################################
+
+-- Sequence: m_defense_incendie.lt_pei_raccord_seq
+
+-- DROP SEQUENCE m_defense_incendie.lt_pei_raccord_seq;
+
+CREATE SEQUENCE m_defense_incendie.lt_pei_raccord_seq
+  INCREMENT 1
+  MINVALUE 0
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+ALTER TABLE m_defense_incendie.lt_pei_raccord_seq
+  OWNER TO sig_create;
+GRANT ALL ON SEQUENCE m_defense_incendie.lt_pei_raccord_seq TO sig_create;
+GRANT SELECT, USAGE ON SEQUENCE m_defense_incendie.lt_pei_raccord_seq TO public;
+
