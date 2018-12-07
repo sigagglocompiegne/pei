3746dfab46393078317d61aab47d2e6b050f827f
gregbodet <37414914+gregbodet@users.noreply.github.com>
2018-12-07 15:23:48 +0100

    Update pei_50_index.sql

--- sql/pei_50_index.sql
+++ sql/pei_50_index.sql
@@ -1,4 +1,11 @@
+/*PEI V1.0*/
+/*Creation des indexs */
+/* pei_50_index.sql */
+/*PostGIS*/
 
+/* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
+/* Auteur : FLorent Vanhoutte */
+/* Participant : Grégory Bodet */
 
 
 -- #################################################################### Point d'eau incendie ####################################################  
