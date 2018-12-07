c66c2e5a734bebd999cae66c485408949e85ce41
gregbodet <37414914+gregbodet@users.noreply.github.com>
2018-12-07 15:31:30 +0100

    Update pei_62_vues_xapps_public.sql

--- sql/pei_62_vues_xapps_public.sql
+++ sql/pei_62_vues_xapps_public.sql
@@ -1,1 +1,10 @@
+/*PEI V1.0*/
+/*Creation des vues applicatives gd public stockées dans le schéma x_apps_public */
+/* pei_6_vues_xapps_pu.sql */
+/*PostGIS*/
 
+/* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
+/* Auteur : FLorent Vanhoutte */
+/* Participant : Grégory Bodet */
+
+-- Sans objet ici
