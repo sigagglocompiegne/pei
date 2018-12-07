df5049c908b3c6aae137d284228a76c8ff70cc36
gregbodet <37414914+gregbodet@users.noreply.github.com>
2018-12-07 15:09:03 +0100

    Update pei_20_seq.sql

--- sql/pei_20_seq.sql
+++ sql/pei_20_seq.sql
@@ -1,1 +1,8 @@
+/*PEI V1.0*/
+/*Creation des séquences intervenant dans l'incrémentation de certaines variables */
+/* pei_20_seq.sql */
+/*PostGIS*/
 
+/* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
+/* Auteur : FLorent Vanhoutte */
+/* Participant : Grégory Bodet */
