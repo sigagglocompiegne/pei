dafe893242c771b8715aef598ce21a916619e259
gregbodet <37414914+gregbodet@users.noreply.github.com>
2018-12-07 15:34:07 +0100

    Update pei_70_triggers.sql

--- sql/pei_70_triggers.sql
+++ sql/pei_70_triggers.sql
@@ -1,1 +1,8 @@
+/*PEI V1.0*/
+/*Creation des triggers et fonctions triggers */
+/* pei_70_triggers.sql */
+/*PostGIS*/
 
+/* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
+/* Auteur : FLorent Vanhoutte */
+/* Participant : Grégory Bodet */
