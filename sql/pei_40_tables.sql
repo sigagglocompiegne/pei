85c5828b6b4ba0e8478811a0c1295db43c0298e4
gregbodet <37414914+gregbodet@users.noreply.github.com>
2018-12-07 15:16:16 +0100

    Update pei_40_tables.sql

--- sql/pei_40_tables.sql
+++ sql/pei_40_tables.sql
@@ -1,1 +1,16 @@
+/*PEI V1.0*/
+/*Creation des tables  */
+/* pei_40_tables.sql */
+/*PostGIS*/
+
+/* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
+/* Auteur : FLorent Vanhoutte */
+/* Participant : Grégory Bodet */
+
+
+DROP TABLE IF EXISTS m_defense_incendie.an_pei_ctr;
+DROP TABLE IF EXISTS m_defense_incendie.geo_pei;
+DROP TABLE IF EXISTS m_defense_incendie.log_pei;
+
+
 
