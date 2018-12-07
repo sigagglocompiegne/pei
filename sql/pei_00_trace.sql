bfb040826f6fd702c1602cee0a4bc2c8722b8b89
gregbodet <37414914+gregbodet@users.noreply.github.com>
2018-12-07 14:48:41 +0100

    Update pei_00_trace.sql

--- sql/pei_00_trace.sql
+++ sql/pei_00_trace.sql
@@ -1,3 +1,12 @@
+/*PEI V1.0*/
+/*Creation du fichier trace qui permet de suivre l'évolution du code*/
+/* pei_10_trace.sql */
+/*PostGIS*/
+
+/* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
+/* Auteur : FLorent Vanhoutte */
+/* Participant : Grégory Bodet */
+
 /*  
   dépendances : voir script d'initialisation des dépendances init_bd_pei_dependencies.sql
   
