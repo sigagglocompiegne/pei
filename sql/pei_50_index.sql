d94d6f496084c4f7c231d1c38946d6407f1c4af3
gregbodet <37414914+gregbodet@users.noreply.github.com>
2018-12-07 15:23:07 +0100

    Update pei_50_index.sql

--- sql/pei_50_index.sql
+++ sql/pei_50_index.sql
@@ -1,1 +1,22 @@
 
+
+
+-- #################################################################### Point d'eau incendie ####################################################  
+  
+-- Index: m_defense_incendie.geo_pei_geom
+
+-- DROP INDEX m_defense_incendie.geo_pei_geom;
+
+CREATE INDEX sidx_geo_pei_geom
+  ON m_defense_incendie.geo_pei
+  USING gist
+  (geom);
+
+-- Index: m_defense_incendie.geo_pei_geom1
+
+-- DROP INDEX m_defense_incendie.geo_pei_geom1;
+
+CREATE INDEX sidx_geo_pei_geom1
+  ON m_defense_incendie.geo_pei
+  USING gist
+  (geom1);
