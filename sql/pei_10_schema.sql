b35ee47c09dda90c83b5025891b7379c869794fe
gregbodet <37414914+gregbodet@users.noreply.github.com>
2018-12-07 15:19:29 +0100

    Update pei_10_schema.sql

--- sql/pei_10_schema.sql
+++ sql/pei_10_schema.sql
@@ -16,17 +16,6 @@
 CREATE SCHEMA m_defense_incendie
   AUTHORIZATION sig_create;
 
-GRANT USAGE ON SCHEMA m_defense_incendie TO edit_sig;
-GRANT ALL ON SCHEMA m_defense_incendie TO sig_create;
-GRANT ALL ON SCHEMA m_defense_incendie TO create_sig;
-GRANT USAGE ON SCHEMA m_defense_incendie TO read_sig;
-ALTER DEFAULT PRIVILEGES IN SCHEMA m_defense_incendie
-GRANT ALL ON TABLES TO create_sig;
-ALTER DEFAULT PRIVILEGES IN SCHEMA m_defense_incendie
-GRANT INSERT, SELECT, UPDATE, DELETE ON TABLES TO edit_sig;
-ALTER DEFAULT PRIVILEGES IN SCHEMA m_defense_incendie
-GRANT SELECT ON TABLES TO read_sig;
-
 COMMENT ON SCHEMA m_defense_incendie
   IS 'Données géographiques métiers sur le thème de la défense incendie';
 
