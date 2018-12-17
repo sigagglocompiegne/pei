2f304d885a4bc977e4925f3a36746f2e1e04859e
gregbodet <37414914+gregbodet@users.noreply.github.com>
2018-12-17 15:44:45 +0100

    Update pei_10_squelette.sql

--- sql/pei_10_squelette.sql
+++ sql/pei_10_squelette.sql
@@ -20,6 +20,14 @@ COMMENT ON SCHEMA m_defense_incendie
   IS 'Données géographiques métiers sur le thème de la défense incendie';
 */
 
+
+-- ####################################################################################################################################################
+-- ###                                                                                                                                              ###
+-- ###                                                                SEQUENCE                                                           ###
+-- ###                                                                                                                                              ###
+-- ####################################################################################################################################################
+
+
 DROP SEQUENCE IF EXISTS m_defense_incendie.geo_pei_id_seq;
 DROP SEQUENCE IF EXISTS m_defense_incendie.log_pei_id_seq;
 DROP SEQUENCE IF EXISTS m_defense_incendie.lt_pei_delegat_seq;
@@ -116,3 +124,51 @@ CREATE SEQUENCE m_defense_incendie.log_pei_id_seq
   MAXVALUE 9223372036854775807
   START 1
   CACHE 1;
+ 
+
+-- ####################################################################################################################################################
+-- ###                                                                                                                                              ###
+-- ###                                                                DOMAINES DE VALEURS                                                           ###
+-- ###                                                                                                                                              ###
+-- ####################################################################################################################################################
+
+
+  -- fkey
+ALTER TABLE m_defense_incendie.geo_pei DROP CONSTRAINT IF EXISTS lt_pei_type_pei_fkey;
+ALTER TABLE m_defense_incendie.geo_pei DROP CONSTRAINT IF EXISTS lt_pei_diam_pei_fkey;
+ALTER TABLE m_defense_incendie.geo_pei DROP CONSTRAINT IF EXISTS lt_pei_source_pei_fkey;
+ALTER TABLE m_defense_incendie.geo_pei DROP CONSTRAINT IF EXISTS lt_pei_statut_fkey;
+ALTER TABLE m_defense_incendie.geo_pei DROP CONSTRAINT IF EXISTS lt_pei_gestion_fkey;
+ALTER TABLE m_defense_incendie.geo_pei DROP CONSTRAINT IF EXISTS lt_pei_etat_pei_fkey;
+ALTER TABLE m_defense_incendie.geo_pei DROP CONSTRAINT IF EXISTS lt_pei_cs_sdis_fkey;
+ALTER TABLE m_defense_incendie.geo_pei DROP CONSTRAINT IF EXISTS lt_pei_marque_fkey;
+ALTER TABLE m_defense_incendie.geo_pei DROP CONSTRAINT IF EXISTS lt_pei_raccord_fkey;
+ALTER TABLE m_defense_incendie.geo_pei DROP CONSTRAINT IF EXISTS lt_pei_delegat_fkey;
+ALTER TABLE m_defense_incendie.geo_pei DROP CONSTRAINT IF EXISTS lt_pei_src_geom_fkey;
+ALTER TABLE m_defense_incendie.an_pei_ctr DROP CONSTRAINT IF EXISTS lt_pei_id_contrat_fkey;
+ALTER TABLE m_defense_incendie.an_pei_ctr DROP CONSTRAINT lt_pei_etat_anom_fkey;
+ALTER TABLE m_defense_incendie.an_pei_ctr DROP CONSTRAINT lt_pei_etat_acces_fkey;
+ALTER TABLE m_defense_incendie.an_pei_ctr DROP CONSTRAINT lt_pei_etat_sign_fkey;
+ALTER TABLE m_defense_incendie.an_pei_ctr DROP CONSTRAINT lt_pei_etat_conf_fkey; 
+
+-- domaine de valeur
+DROP TABLE IF EXISTS m_defense_incendie.lt_pei_anomalie;
+DROP TABLE IF EXISTS m_defense_incendie.lt_pei_cs_sdis;
+DROP TABLE IF EXISTS m_defense_incendie.lt_pei_delegat;
+DROP TABLE IF EXISTS m_defense_incendie.lt_pei_diam_pei;
+DROP TABLE IF EXISTS m_defense_incendie.lt_pei_etat_boolean;
+DROP TABLE IF EXISTS m_defense_incendie.lt_pei_etat_pei;
+DROP TABLE IF EXISTS m_defense_incendie.lt_pei_gestion;
+DROP TABLE IF EXISTS m_defense_incendie.lt_pei_id_contrat;
+DROP TABLE IF EXISTS m_defense_incendie.lt_pei_marque;
+DROP TABLE IF EXISTS m_defense_incendie.lt_pei_raccord;
+DROP TABLE IF EXISTS m_defense_incendie.lt_pei_source_pei;
+DROP TABLE IF EXISTS m_defense_incendie.lt_pei_statut;
+DROP TABLE IF EXISTS m_defense_incendie.lt_pei_type_pei;
+
+
+
+-- ################################################################# Domaine valeur - type_pei  ###############################################
+
+-- Table: m_defense_incendie.lt_pei_type_pei
+
