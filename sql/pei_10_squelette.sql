18f4929e2054905d66c5b54bd83bd3681cb6b29b
gregbodet <37414914+gregbodet@users.noreply.github.com>
2018-12-17 15:58:49 +0100

    Update pei_10_squelette.sql

--- sql/pei_10_squelette.sql
+++ sql/pei_10_squelette.sql
@@ -1048,8 +1048,30 @@ ALTER TABLE m_defense_incendie.an_pei_ctr
       REFERENCES m_defense_incendie.lt_pei_etat_boolean (code) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION; 
 
+-- ####################################################################################################################################################
+-- ###                                                                                                                                              ###
+-- ###                                                                INDEX                                                           ###
+-- ###                                                                                                                                              ###
+-- ####################################################################################################################################################
 
 
+-- Index: m_defense_incendie.geo_pei_geom
+
+-- DROP INDEX m_defense_incendie.geo_pei_geom;
+
+CREATE INDEX sidx_geo_pei_geom
+  ON m_defense_incendie.geo_pei
+  USING gist
+  (geom);
+
+-- Index: m_defense_incendie.geo_pei_geom1
+
+-- DROP INDEX m_defense_incendie.geo_pei_geom1;
+
+CREATE INDEX sidx_geo_pei_geom1
+  ON m_defense_incendie.geo_pei
+  USING gist
+  (geom1);
 
 
 
